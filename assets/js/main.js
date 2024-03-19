032a4ffb1c9ae6092a059567b9854814ef9bcefa
StefDev1801 <stefano200118@gmail.com>
2024-03-19 16:57:24 +0100

    add v-if to display only if all email are present

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,21 @@ createApp({
           arrayEmail : [],
         }
     },
-    mounted(){
-        for (let i = 0; i < 10; i++) {
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response)=>{
-                console.log(response);
-                console.log(response.data.response);//will give me a rnd email
-                this.arrayEmail.push(response.data.response)
-        })
+    methods : {
+        callApi(){
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response)=>{
+                        console.log(response.data.response);//will give me a rnd email
+                        this.arrayEmail.push(response.data.response)
+                        console.log(this.arrayEmail);
+                })
+                }
         }
+    },
+    mounted(){
+        this.callApi()
     }
 }).mount('#app')
 
